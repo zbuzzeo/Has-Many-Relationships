20a3a21789d934e3eaa1fad198fa60e12590c82a
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-02-01 19:38:41 -1000

    Exercise 5: Usernames of posts made after Jan 2015

--- joins.sql
+++ joins.sql
@@ -11,3 +11,6 @@ WHERE user_id = 200;
 SELECT posts.id, posts.user_id, posts.title, posts.url, posts.content, posts.created_at, posts.updated_at,users.username
 FROM posts INNER JOIN users ON users.id = posts.user_id
 WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
+
+SELECT users.username FROM users INNER JOIN posts ON users.id = posts.user_id
+WHERE posts.created_at > date '2015-01-01';
