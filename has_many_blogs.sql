00b4b9d4052dbe89d77c2418c958618e534d43aa
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-02-01 19:29:42 -1000

    first four queries, work with INNER JOIN posts and users

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,33 +1,40 @@
+DROP DATABASE IF EXISTS has_many_blogs;
+DROP user IF EXISTS has_many_user;
+
 CREATE user has_many_user;
 CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
 
+\connect has_many_blogs;
+
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
-  id serial NOT NULL,
+  id SERIAL PRIMARY KEY,
   username varchar(90) NOT NULL,
   first_name varchar(90) default NULL,
   last_name varchar(90) default NULL,
-  created_at timestamp NOT NULL default now(),
-  updated_at timestamp NOT NULL default now()
+  created_at TIMESTAMP WITH TIME ZONE default now(),
+  updated_at TIMESTAMP WITH TIME ZONE default now()
 );
 
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts(
-  id serial NOT NULL,
+  id SERIAL PRIMARY KEY,
   title varchar(180) default NULL,
   url varchar(510) default NULL,
   content text default NULL,
-  created_at timestamp NOT NULL default now(),
-  updated_at timestamp NOT NULL default now()
+  created_at TIMESTAMP WITH TIME ZONE default now(),
+  updated_at TIMESTAMP WITH TIME ZONE default now(),
+  user_id INTEGER NOT NULL REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS comments;
 CREATE TABLE comments(
-  id serial NOT NULL,
+  id SERIAL PRIMARY KEY,
   body varchar(510) default NULL,
-  created_at timestamp NOT NULL default now(),
-  updated_at timestamp NOT NULL default now()
+  created_at TIMESTAMP WITH TIME ZONE default now(),
+  updated_at TIMESTAMP WITH TIME ZONE default now(),
+  user_id INTEGER NOT NULL REFERENCES users(id),
+  post_id INTEGER NOT NULL REFERENCES posts(id)
 );
 
 \i scripts/blog_data.sql;
--- \i ./joins.sql;
--- joins.sql
+++ joins.sql
@@ -1,1 +1,13 @@
+SELECT * FROM users;
 
+SELECT * FROM posts
+WHERE user_id = 100;
+
+SELECT 
+posts.id, posts.user_id, posts.title, posts.url, posts.content, posts.created_at, posts.updated_at, users.first_name, users.last_name
+FROM posts INNER JOIN users ON users.id = posts.user_id 
+WHERE user_id = 200;
+
+SELECT posts.id, posts.user_id, posts.title, posts.url, posts.content, posts.created_at, posts.updated_at,users.username
+FROM posts INNER JOIN users ON users.id = posts.user_id
+WHERE user.first_name = 'NORENE' AND user.last_name 'SCHMITT';
